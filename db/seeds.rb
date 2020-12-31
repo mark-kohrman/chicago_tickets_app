f485d30e33ac99c14539aec944bb549af6619fe4
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-30 21:16:44 -0600

    add ticket data file and loop thru seeds file and add to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'public_urination_tickets.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Ticket.new
+  t.date = row['NOV_ISSUED_DATE']
+  t.ticket_no = row['NOV_NUMBER']
+  t.street_no = row['STREET_NO']
+  t.direction = row['DIR']
+  t.street_name = row['STREET_NME']
+  t.violation_description = row['VIOLATION_DESCRIPTION']
+  t.save
+  puts "This person was issued a ticket on the #{t.street_no} block of #{t.street_name} on #{t.date}"
+end
+puts csv_text
+
